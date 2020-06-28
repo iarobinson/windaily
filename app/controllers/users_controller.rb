7d563f27c45a84c3aeb8a14917349e5f4ac25b23
Ian Robinson <ian@ianrobinson.net>
2020-06-29 00:17:57 +0900

    Add functionality for the user to adjust their name. Route, Controller and View

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,36 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def index
     @users = User.all
   end
+
   def show
     @user = User.find(params[:id])
   end
+
+  def update
+    # We want to attache the avatar image here
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+    def set_user
+      @user = current_user
+    end
+
+    def user_params
+      params.fetch(:user, {}).permit(
+        :first_name, :last_name, :avatar
+      )
+    end
 end
