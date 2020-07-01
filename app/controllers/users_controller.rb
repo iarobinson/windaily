5b3641efcfc3e2791aee1d1bbfc710fa4ad6a031
Ian Robinson <ian@ianrobinson.net>
2020-07-01 09:54:26 -0700

    Update database configuration Add some mailer testing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     respond_to do |format|
       if @user.save
         UserMailer.with(user: @user).welcome_email.deliver_later
-        format.html { redirect_to(@user, notice 'User was successfully created.') }
+        format.html { redirect_to(@user, notice: 'User was successfully created.') }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: 'new' }
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,12 @@
 class UserMailer < ApplicationMailer
-  default from: 'dailywinner@dailywinner.com'
+  default from: 'team@windaily.com'
 
   def welcome_email
     @user = params[:user]
     @url = 'http://dailywinner.com/login'
-    mail(to: @user.email, subject: "Welcome to Daily Winner. A place where you work with your frineds to develop good habits.")
+    mail(
+      to: @user.email,
+      subject: "Welcome to Daily Winner. A place where you work with your frineds to develop good habits."
+    )
   end
 end
