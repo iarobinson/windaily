3ff259bde09d61df156bce66b92b610591b99de0
Ian Robinson <ian@ianrobinson.net>
2020-08-04 17:53:15 -0700

    Fix mailer error

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,30 +5,29 @@ class UserMailer < ApplicationMailer
   default from: 'victory@windaily.app'
 
   def notify_others_of_win(win)
-
-    binding.pry
-    win.challenge.users do |user|
-
-      email_text = """
-      #{win.user.first_name} #{win.user.last_name} (#{win.user.email}) got a win.
-
-      #{win.title}
-
-      #{win.description}
-
-      #{challenges_url(win.challenge)}
-      """
-
-      from = Email.new(email: "victory@windaily.app", name: "Win Daily")
-      subject = `#{win.user.first_name} #{win.user.last_name} (#{win.user.email}) got a win`
-      to = Email.new(email: user.email)
-      content = Content.new(type: 'text/plain', value: email_text)
-      mail = SendGrid::Mail.new(from, subject, to, content)
-
-      sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-      response = sg.client.mail._('send').post(request_body: mail.to_json)
-      p `email sent to #{user.email}`
-    end
+    #
+    # win.challenge.users do |user|
+    #
+    #   email_text = """
+    #   #{win.user.first_name} #{win.user.last_name} (#{win.user.email}) got a win.
+    #
+    #   #{win.title}
+    #
+    #   #{win.description}
+    #
+    #   #{win.challenge.id}
+    #   """
+    #
+    #   from = Email.new(email: "victory@windaily.app", name: "Win Daily")
+    #   subject = `#{win.user.first_name} #{win.user.last_name} (#{win.user.email}) got a win`
+    #   to = Email.new(email: user.email)
+    #   content = Content.new(type: 'text/plain', value: email_text)
+    #   mail = SendGrid::Mail.new(from, subject, to, content)
+    #
+    #   sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+    #   response = sg.client.mail._('send').post(request_body: mail.to_json)
+    #   p `email sent to #{user.email}`
+    # end
   end
 
   def you_have_been_challenged_email(challenger, challenged, challenge, temporary_password)
@@ -46,13 +45,13 @@ class UserMailer < ApplicationMailer
 
     You have been challenged by a user with the following email: #{challenger.email}
 
-    To accept this challenge, visit #{challenges_path(challenge)}.
+    Click here to accept this challenge, visit .
 
     Your login email is: #{challenged.email}
 
     Your login password is: #{temporary_password}
 
-    https://www.windaily.app
+    https://www.windaily.app/challenges/#{challenge.id.to_s}
     """
 
     from = Email.new(email: "victory@windaily.app", name: "Win Daily")
