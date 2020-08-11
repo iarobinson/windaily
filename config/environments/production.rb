ebd206a1a7780e7ec7668c22838cb96d17ddcef4
Ian Robinson <ian@ianrobinson.net>
2020-08-11 15:33:22 -0700

    more production config variables attempts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,15 +69,14 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.default_url_options = { host: 'windaily.app' }
   ActionMailer::Base.smtp_settings = {
-    user_name:            ENV['SENDGRID_USERNAME'],
-    password:             ENV['SENDGRID_PASSWORD'],
-    address:              "smtp.sendgrid.net",
-    port:                 587,
-    enable_starttls_auto: true,
-    authentication:       :plain,
-    domain:               'windaily.app'
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
   }
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
