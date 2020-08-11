dc6b469f8c0c8ff2944a32b8320b1d38aaf28598
Ian Robinson <ian@ianrobinson.net>
2020-08-11 14:18:50 -0700

    Adjust configuration so devise can use sendgrid

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,17 @@ Rails.application.configure do
   # config.action_mailer.perform_caching = false
   #
   config.action_mailer.default_url_options = { host: "https://www.windaily.app/"}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'windaily.app' }
+  ActionMailer::Base.smtp_settings = {
+    address:         "smtp.sendgrid.net",
+    port:            25,
+    user_name:       ENV['SENDGRID_USERNAME'],
+    password:        ENV['SENDGRID_PASSWORD'],
+    domain:          ENV['SENDGRID_DOMAIN'],
+    authentication:  :plain
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
