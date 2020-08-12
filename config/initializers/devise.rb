611797c5748b80f039cee47625dc13b4456ddb4c
Ian Robinson <ian@ianrobinson.net>
2020-08-12 14:33:39 -0700

    Still tweaking config to get mailer working in production for devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -19,7 +19,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "mail-to-send@from.com"
+  config.mailer_sender = "victory@windaily.app"
   config.reconfirmable = true
 
   # Configure the class responsible to send e-mails.
