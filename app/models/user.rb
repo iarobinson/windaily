466325da1f578598752ea56689e86f7fddb8b7bc
Ian Robinson <ian@ianrobinson.net>
2020-08-12 19:14:38 -0700

    We need to ensure that when the User is destroyed, their wins and avatar goes with them.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,8 @@ class User < ApplicationRecord
          # :confirmable, :lockable, :timeoutable, :trackable
 
    has_and_belongs_to_many :challenges
-   has_one_attached :avatar
-   has_many :wins
+   has_one_attached :avatar, dependent: :destroy
+   has_many :wins, dependent: :destroy
 
    def thumbnail
      return self.avatar.variant(resize: "150x150!").processed
