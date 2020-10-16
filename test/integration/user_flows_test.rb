ef9b8e5e2c935980551a391b2e09bf620775120a
Ian Robinson <ian@ianrobinson.net>
2020-10-16 17:38:10 -0500

    Add a user sign in test

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -2,9 +2,19 @@ require 'test_helper'
 
 class UserFlowsTest < ActionDispatch::IntegrationTest
 
+  setup do
+    @user = users(:ian)
+  end
+
   test "visit root path" do
     visit root_path
     click_link('Sign In', match: :first)
-    page.has_content? "Log in"
+    within('form#new_user') do
+      fill_in("user_email", with: @user.email)
+      fill_in("user_password", with: @user.password)
+    end
+    # binding.pry
+    click_button "Log in"
+    page.has_content? "Win Daily! Signed in successfully."
   end
 end
